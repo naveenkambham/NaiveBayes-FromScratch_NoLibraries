e797ef1493d25d007d9b87058ad306f0497ceece
Naveen <naveen.kumar6015@gmail.com>
2018-03-07 19:21:03 -0600

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,8 +1,6 @@
 """
 Title:Implement Naive Bayes Classifier and run various test cases like using MLE, MAP estimates to understand the algorithm.
 Developer : Naveen Kambham
-ID:nkk300
-Purpose: Course Assignment
 Description: This is a simple Naive Bayes classifier implemented using MAP and MLE estimates for parameters. It has various test cases like
              using MLE estimates, MAP estimates and using the Leave One out cross validation to check the error rates.
 """
